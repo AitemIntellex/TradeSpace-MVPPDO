63f832801b52cd8d9eb46b4b69408c5de9f00826
DaveMinchen <aitemintellex@gmail.com>
2025-05-21 15:39:45 +0500

    Удалены package-lock.json и Temp/ из Git, добавлены в .gitignore

--- Temp/webapp2/temp/src/utils/test.py
+++
@@ -1,8 +0,0 @@
-def test_symbol_info(symbol):
-    import MetaTrader5 as mt5
-
-    info = mt5.symbol_info(symbol)
-    if info is None:
-        print(f"Не удалось получить информацию для {symbol}")
-    else:
-        print(f"Информация о символе {symbol}: {info}")
